949cf9f1e75cec479e4703b8d8d440553758dfe3
Breno F. Rodrigues <brenoferreira.bf8@gmail.com>
2022-05-13 03:22:52 -0300

    commit

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Breno7k/The-famous-to-do-list.git main:gh-pages
+git push -f git@github.com:Breno7k/The-famous-to-do-list.git master:gh-pages
 
 cd -
