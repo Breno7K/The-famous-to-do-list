f629a0fa5b15a5c8a5f19d4698d4fb1903d84aba
Breno F. Rodrigues <brenoferreira.bf8@gmail.com>
2022-05-31 12:22:20 -0300

    gh p

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
 
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/The-famous-to-do-list/'
-    : '/',
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/gh-to-do-list/'
+  : '/',
   transpileDependencies: true
 })
